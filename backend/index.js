d5058e2b203620cb84d86be97fb7b14e64a982aa
tlenglin <tlenglin@student.42.fr>
2019-01-30 15:36:13 +0100

    auth with passport in REST API

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,63 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const users = require('./routes/user')
+const express 		= require('express');
+const logger 	    = require('morgan');
+const bodyParser 	= require('body-parser');
+const passport      = require('passport');
+const pe            = require('parse-error');
+const cors          = require('cors');
+const v1 = require('./routes/v1');
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = express();
 
-mongoose.connect(
-  'mongodb://127.0.0.1:27017/MERN',
-  { useNewUrlParser: true }
-)
-const connection = mongoose.connection
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully')
-})
+const CONFIG = require('./config/config');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-  res.send('hello')
-})
+//Passport
+app.use(passport.initialize());
 
-app.use('/api/users', users)
+//Log Env
+console.log("Environment:", CONFIG.app)
+//DATABASE
+const models = require("./models");
 
-app.listen(4000, () => {
-  console.log('Server running on port 4000')
-})
+// CORS
+app.use(cors());
+
+app.use('/v1', v1);
+
+app.use('/', function(req, res){
+	res.statusCode = 200;//send the appropriate status code
+	res.json({status:"success", message:"Mongo API", data:{}})
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+process.on('unhandledRejection', error => {
+    console.error('Uncaught Error', pe(error));
+});
+
+app.listen(CONFIG.port, err => {
+  if(err) console.error(err);
+  console.log(`Listening for Requests on port: ${CONFIG.port}`);
+}); 
